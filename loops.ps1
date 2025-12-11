baec6327239c163eaac373f10c93277a30cfd0af
gautam <sandipgautam263@gmail.com>
2025-12-11 13:59:06 +0545

    Loops in shell

--- loops.ps1
+++ loops.ps1
@@ -5,3 +5,25 @@
 for ($i=0; $i -lt 5; $i++){
   Write-Host "This is a for loop" 
 }
+
+
+# 2. While Loop
+$k=1
+while ( $k -lt 5){
+  Write-Host "This is a while loops"
+  $k++
+}
+
+# Do-While loop
+
+$m=1
+do { 
+  Write-Host "This is a do-while loop"
+  $m++
+} while ( $m -lt 5)     
+
+# 4. ForEach Loop
+$colors = @("Red", "Green", "Blue")
+foreach ($color in $colors){
+  Write-Host "Color: $color"
+}
